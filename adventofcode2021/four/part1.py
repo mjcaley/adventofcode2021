8f0322cd07197da3200a8890f7bb6d9585c70dcf
Michael Caley <mjcaley@darkarctic.com>
2021-12-05 22:12:16 -0500

    Finished day 4

---
+++ adventofcode2021/four/part1.py
@@ -0,0 +1,103 @@
+from dataclasses import dataclass
+from functools import reduce
+import sys
+from io import StringIO
+from itertools import accumulate, islice
+
+
+def parse_called(line):
+    return [int(number) for number in line.split(",")]
+
+
+@dataclass
+class Square:
+    number: int
+    called: bool = False
+
+    def __repr__(self):
+        return f"{self.number}:{self.called}"
+
+
+def parse_board_line(line):
+    return [Square(int(number)) for number in line.strip().split(" ") if number]
+
+
+def mark(board, number):
+    for square in board:
+        if square.number == number:
+            square.called = True
+
+
+def is_winner(board):
+    """
+    1  2  3  4  5
+    6  7  8  9  10
+    11 12 13 14 15
+    16 17 18 19 20
+    21 22 23 24 25
+
+    iterate over rows = start * 5 to start * 5 + 5
+    iterate over columns = 0 to end, step 5
+    """
+
+    # Check rows
+    for row_start in range(5):
+        row = [square for square in islice(board, row_start * 5, row_start * 5 + 5)]
+        if all([square.called for square in row]):
+            return True
+    
+    # Check columns
+    for column_start in range(5):
+        column = [square for square in islice(board, column_start, None, 5)]
+        if all([square.called for square in column]):
+            return True
+
+    return False
+
+def call(boards, number):
+    """Will mark the number on all boards. If a board wins it will be returned."""
+
+    for board in boards:
+        mark(board, number)
+        if is_winner(board):
+            return board
+
+
+def sum_unmarked(board):
+    total = 0
+    for square in board:
+        if not square.called:
+            total += square.number
+
+    return total
+
+
+def part1(data):
+    boards = []
+
+    with StringIO(data) as file:
+        called_numbers = parse_called(file.readline())
+        file.readline()
+
+        board = []
+        for line in file:
+            if line == "\n":
+                boards.append(board)
+                board = []
+            else:
+                board += parse_board_line(line)
+
+    for number in called_numbers:
+        if winning_board := call(boards, number):
+            break
+
+    unmarked = sum_unmarked(winning_board)
+
+    return unmarked, number, unmarked * number
+
+
+if __name__ == "__main__":
+    with open(sys.argv[1], "r") as file:
+        data = file.read()
+
+    print(part1(data))
